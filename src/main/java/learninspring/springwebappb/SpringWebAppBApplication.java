2ead93d4f91e4d43adb81fbfa36e4d0c231b8ad1
mlanima <danylokonovalenko49@gmail.com>
2024-08-27 17:56:25 +0200

    Initial commit

---
+++ src/main/java/learninspring/springwebappb/SpringWebAppBApplication.java
@@ -0,0 +1,13 @@
+package learninspring.springwebappb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAppBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learninspring/springwebappb/SpringWebAppBApplicationTests.java
@@ -0,0 +1,13 @@
+package learninspring.springwebappb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
